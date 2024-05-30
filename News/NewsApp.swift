b30b3398ebad7774dc60412b14f10e4c73f53508
Arshif1 <mohammedarshif456@gmail.com>
2024-05-30 18:17:52 +0400

    Initial Commit

---
+++ News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Arshif on 30/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
