502d4d4cc935dc75b8b02b378e67c0f8033025b8
Arshif1 <mohammedarshif456@gmail.com>
2024-06-19 19:27:22 +0400

    some changes news cell

---
+++ News_Uikit/NewsCell.swift
@@ -0,0 +1,23 @@
+//
+//  NewsCell.swift
+//  News_Uikit
+//
+//  Created by Arshif on 19/06/2024.
+//
+
+import UIKit
+
+class NewsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
