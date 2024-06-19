e23ccfcb02a5c56803f33c68f6e1ee1e23ed89c8
Arshif1 <mohammedarshif456@gmail.com>
2024-06-19 20:58:37 +0400

    setup color and font for newsCell

--- News_Uikit/NewsCell.swift
+++ News_Uikit/NewsCell.swift
@@ -8,16 +8,35 @@
 import UIKit
 
 class NewsCell: UITableViewCell {
-
+    
+    @IBOutlet private weak var newsImageView: UIImageView!
+    @IBOutlet private weak var labelTitle: UILabel!
+    @IBOutlet private weak var labelPublishedBy: UILabel!
+    @IBOutlet private weak var labelCategory: UILabel!
+    @IBOutlet private weak var labelDate: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        setupViews()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    private func setupViews() {
+        
+        let categoryColor = UIColor(red: 105/255, green: 189/255, blue: 253/255, alpha: 1)
+        let titleColor = UIColor(red: 24/255, green: 14/255, blue: 25/255, alpha: 1)
+        let publishedByColor = UIColor(red: 144/255, green: 144/255, blue: 144/255, alpha: 1)
+        let dateColor = UIColor(red: 144/255, green: 144/255, blue: 144/255, alpha: 1)
+        
+        
+        labelTitle.font = UIFont.systemFont(ofSize: 14, weight: .bold)
+        labelPublishedBy.font = UIFont.systemFont(ofSize: 13, weight: .medium)
+        labelDate.font = UIFont.systemFont(ofSize: 13, weight: .medium)
+        labelCategory.font = UIFont.systemFont(ofSize: 13, weight: .bold)
+        
+        labelTitle.textColor = titleColor
+        labelPublishedBy.textColor = publishedByColor
+        labelCategory.textColor = categoryColor
+        labelDate.textColor = dateColor
+        
     }
-
 }
--- News_Uikit/ViewController.swift
+++ News_Uikit/ViewController.swift
@@ -7,13 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupTableView()
     }
 
-
+    private func setupTableView() {
+        tableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "NewsCell", for: indexPath)
+        return cell
+    }
 }
 
