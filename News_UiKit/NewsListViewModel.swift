9895fc424bbcee75651136f5902134e16aadb212
Arshif <mohammedarshif456@gmail.com>
2024-08-13 20:48:23 +0400

    create and use viewmodel

---
+++ News_UiKit/NewsListViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  NewsListViewModel.swift
+//  News_UiKit
+//
+//  Created by Azhar on 13/08/2024.
+//
+
+import Foundation
+import News_Package
+
+class NewsListViewModel {
+    
+    var onarticlesLoadedClosure: (([Article]) -> Void)?
+    
+    private let articleLoader = ArticleLoader()
+    
+    func loadNews() {
+        Task {
+            guard let loadedArticles = try? await articleLoader.loadArticles() else { return }
+            await MainActor.run {
+                onarticlesLoadedClosure?(loadedArticles)
+            }
+        }
+    }
+    
+    func load(with searchText: String) {
+        Task {
+            guard let loadedArticles = try? await articleLoader.load(With: searchText) else { return }
+            await MainActor.run {
+                onarticlesLoadedClosure?(loadedArticles)
+            }
+        }
+    }
+    
+}
