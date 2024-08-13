b30576392eea687af21892155ba7edbf0c6efe3d
Arshif <mohammedarshif456@gmail.com>
2024-08-13 20:13:30 +0400

    Create Uikit target

---
+++ News_UiKit/NewsListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  NewsListViewController.swift
+//  News_UiKit
+//
+
+import UIKit
+
+class NewsListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
