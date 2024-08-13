d411aee08ea29c77ee311c1587341baf8e204a79
Arshif <mohammedarshif456@gmail.com>
2024-08-13 20:19:23 +0400

    implement table view with some mock data

--- News_UiKit/NewsListViewController.swift
+++ News_UiKit/NewsListViewController.swift
@@ -5,11 +5,39 @@
 
 import UIKit
 
+struct Article {
+    let title = "Atitle"
+}
+
 class NewsListViewController: UIViewController {
+    
+    @IBOutlet private weak var tableView: UITableView?
+    
+    private var articles: [Article] = Array(repeating: .init(), count: 20)
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configure()
     }
 
+    private func configure() {
+        tableView?.dataSource = self
+        tableView?.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+}
+
+
+extension NewsListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return articles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = articles[indexPath.row].title
+        return cell
+    }
+    
 
 }
