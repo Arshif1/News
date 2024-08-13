e4ce8c9df06c811accea394d4985e44ac20be293
Arshif <mohammedarshif456@gmail.com>
2024-08-13 21:12:55 +0400

    Rename News Package to News

--- News/Package.swift
+++ News/Package.swift
@@ -4,21 +4,18 @@
 import PackageDescription
 
 let package = Package(
-    name: "News_Package",
+    name: "News",
     platforms: [.iOS("17.0")],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "News_Package",
-            targets: ["News_Package"]),
+            name: "News",
+            targets: ["News"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "News_Package"),
-        .testTarget(
-            name: "News_PackageTests",
-            dependencies: ["News_Package"]),
+            name: "News"),
     ]
 )
