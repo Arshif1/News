5a4ce0f602be1d11186a5e7563cb44549c780c1e
Arshif <mohammedarshif456@gmail.com>
2024-08-13 21:00:37 +0400

    update newslist view controller

--- News_UiKit/NewsListViewController.swift
+++ News_UiKit/NewsListViewController.swift
@@ -4,16 +4,19 @@
 //
 
 import UIKit
-
-struct Article {
-    let title = "Atitle"
-}
+import News_Package
 
 class NewsListViewController: UIViewController {
     
     @IBOutlet private weak var tableView: UITableView?
     
-    private var articles: [Article] = Array(repeating: .init(), count: 20)
+    private var articles: [Article] = [] {
+        didSet {
+            tableView?.reloadData()
+        }
+    }
+    
+    private var viewmodel = NewsListViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,10 +25,17 @@ class NewsListViewController: UIViewController {
 
     private func configure() {
         tableView?.dataSource = self
-        tableView?.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView?.register(UINib(nibName: "ArticleListCell", bundle: .main), forCellReuseIdentifier: "cell")
+        configureViewModel()
+    }
+    
+    private func configureViewModel() {
+        viewmodel.onarticlesLoadedClosure = { [weak self] articles in
+            self?.articles = articles
+        }
+        viewmodel.loadNews()
     }
 }
-
 
 extension NewsListViewController: UITableViewDataSource {
     
@@ -34,10 +44,9 @@ extension NewsListViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = articles[indexPath.row].title
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ArticleListCell
+        cell.labelTitle?.text = articles[indexPath.row].title
+        cell.labelDescription?.text = articles[indexPath.row].description
         return cell
     }
-    
-
 }
